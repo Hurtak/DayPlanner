f5a3dd0ee5b735355156a0378337256797b81d5d
Hurtak <petr.hurtak@gmail.com>
2014-09-01 19:42:53 +0200

    added menu which is displayed after clicking on item

--- src/dayplanner.js
+++ src/dayplanner.js
@@ -1,21 +1,51 @@
 var DayPlanner = function() {
+	var items, menu;
+
+	var openedItemHeight = 200; // px
+	var openedItem;
+
+	var closeItem = function(el) {
+		el.style.height = el.getAttribute("data-duration") + "px";
+	};
+
+	var showMenu = function(item) {
+		item.appendChild(menu);
+		menu.style.display = "block";
+	};
+
+	var hideMenu = function(item) {
+		item.removeChild(menu);
+	};
 
 	var init = function() {
-		var x = document.querySelectorAll('.item');
-		console.log(x);
+		items = document.querySelectorAll('.item');
+		menu = document.getElementById('menu');
 
-		for (var i = 0; i < x.length; i++) {
+		for (var i = 0; i < items.length; i++) {
 			// set height according to duration (1 minute = 1px)
-			x[i].style.height = x[i].getAttribute("data-duration") + "px";
-		console.log(x[i].style.height.slice(0, - 2));
+			items[i].style.height = items[i].getAttribute("data-duration") + "px";
 
-			x[i].onclick = 	function() {
-				if (this.getAttribute("data-duration") === this.style.height.slice(0, - 2)) {
-					this.style.height = 200 + "px";
-				} else {
-					this.style.height = this.getAttribute("data-duration") + "px";
-				}
+			// add onclick to display options
+			items[i].onclick = function() {
+				openItem(this);
 			};
+		}
+	};
+
+	var openItem = function(el) {
+		// click folded on item
+		if (el.getAttribute("data-duration") === el.style.height.slice(0, - 2)) {
+			el.style.height = openedItemHeight + "px";
+			if (openedItem && openedItem !== el) {
+				closeItem(openedItem);
+			}
+			openedItem = el;
+
+			showMenu(el);
+		// click different item, or the same opened item
+		} else {
+			el.style.height = el.getAttribute("data-duration") + "px";
+			hideMenu(el);
 		}
 	};
 
